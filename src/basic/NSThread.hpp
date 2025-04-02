214540d0e6b299874fc0c7ed35577a633a4dc158
0x0-bwu <350240702@qq.com>
2025-04-02 22:33:25 +0800

    fix: via singleton

--- src/basic/NSThread.hpp
+++ src/basic/NSThread.hpp
@@ -3,23 +3,53 @@
 
 namespace nano::thread {
 
-inline static std::atomic_size_t threads = std::thread::hardware_concurrency();
-
 using ThreadPool = generic::thread::ThreadPool;
+
+class ThreadMgr
+{
+public:
+    static ThreadMgr & Instance()
+    {
+        static ThreadMgr instance;
+        return instance;
+    }
+
+    void SetThreads(size_t n)
+    {
+        m_threads.store(n);
+    }
+
+    size_t Threads() const
+    {
+        return m_threads.load();
+    }
+
+    ThreadPool Pool() const
+    {
+        return ThreadPool(Threads());
+    }
+
+private:
+    ThreadMgr() : m_threads(std::thread::hardware_concurrency()) {}
+    ThreadMgr(const ThreadMgr &) = delete;
+    ThreadMgr & operator= (const ThreadMgr &) = delete;
+
+    std::atomic_size_t m_threads;
+};
 
 inline void SetThreads(size_t n)
 {
-    threads.store(n);
+    ThreadMgr::Instance().SetThreads(n);
 }
 
 inline size_t Threads()
 {
-    return threads.load();
+    return ThreadMgr::Instance().Threads();
 }
 
 inline ThreadPool Pool()
 {
-    return ThreadPool(Threads());
+    return ThreadMgr::Instance().Pool();
 }
 
 } // namespace nano::thread
